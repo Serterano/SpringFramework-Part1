fb79a11ba8f70287211c827ecc047a9d50ca0952
Ali <63211639+Serterano@users.noreply.github.com>
2023-01-06 03:28:34 +0300

    Update HelloBean.java

--- SpringFramework002/src/main/java/com/erano/HelloBean.java
+++ SpringFramework002/src/main/java/com/erano/HelloBean.java
@@ -1,13 +1,7 @@
 package com.erano;
 
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.stereotype.Component;
-
-//@Component is not necessary for this project
-@Component
 public class HelloBean {
-	//@Value is not necessary too
-	@Value("name")
+
 	private String name;
 
 	public String getName() {
